ac3fafc05cd4646b9ad9829017915fa111fd58b3
jeremymarch <jmarch@gradcenter.cuny.edu>
2023-11-18 13:10:59 -0500

    start to add in oauth, but commented out for now

--- hc-axum/src/login.rs
+++ hc-axum/src/login.rs
@@ -305,23 +305,370 @@ pub async fn new_user_post(
             //.map_err(map_hc_error)
         {
             Ok(_user_id) => {
-                println!("here1");
                 //session.renew(); //https://www.lpalmieri.com/posts/session-based-authentication-in-rust/#4-5-2-session
                 //if session.insert("user_id", user_id).is_ok() {
                     Redirect::to("/login")
                 //}
             }
             Err(e) => {
-                println!("here2 {:?}", e);
                 //session.purge();
                 //FlashMessage::error(String::from("Create user error")).send();
                 Redirect::to("/newuser")
             }
         }
     } else {
-        println!("here3");
         //session.purge();
         //FlashMessage::error(String::from("Create user error")).send();
         Redirect::to("/newuser")
     }
 }
+
+
+
+
+
+/*
+use actix_web::http::header;
+use libhc::hc_create_oauth_user;
+use oauth2::basic::BasicClient;
+use oauth2::AuthUrl;
+use oauth2::ClientId;
+use oauth2::ClientSecret;
+use oauth2::RedirectUrl;
+use oauth2::ResponseType;
+use oauth2::TokenUrl;
+use oauth2::{AuthorizationCode, CsrfToken, PkceCodeChallenge, Scope};
+use serde::Deserialize;
+use serde::Serialize;
+use std::env;
+
+#[derive(Deserialize)]
+pub struct AuthRequest {
+    code: Option<String>,
+    state: String,
+    id_token: Option<String>,
+    user: Option<String>,
+}
+
+pub struct AppState {
+    pub apple_oauth: BasicClient,
+    pub google_oauth: BasicClient,
+}
+/*
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct AppleClaims {
+    iss: Option<String>,
+    aud: Option<String>,
+    exp: Option<u64>,
+    iat: Option<u64>,
+    sub: Option<String>,
+    c_hash: Option<String>,
+    auth_time: Option<u64>,
+    nonce: Option<String>,
+    nonce_supported: Option<bool>,
+    email: Option<String>,
+    //this is bool for Google and String for Apple
+    //https://developer.apple.com/forums/thread/121411?answerId=378290022#378290022
+    email_verified: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct GoogleClaims {
+    iss: Option<String>,
+    aud: Option<String>,
+    exp: Option<u64>,
+    iat: Option<u64>,
+    sub: Option<String>,
+    c_hash: Option<String>,
+    auth_time: Option<u64>,
+    nonce: Option<String>,
+    nonce_supported: Option<bool>,
+    email: Option<String>,
+    //this is bool for Google and String for Apple
+    //https://developer.apple.com/forums/thread/121411?answerId=378290022#378290022
+    email_verified: Option<bool>,
+}
+*/
+#[derive(Debug, Serialize, Deserialize)]
+struct AppleOAuthUserName {
+    #[serde(rename(serialize = "firstName"), rename(deserialize = "firstName"))]
+    first_name: Option<String>,
+    #[serde(rename(serialize = "lastName"), rename(deserialize = "lastName"))]
+    last_name: Option<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct AppleOAuthUser {
+    name: AppleOAuthUserName,
+    email: Option<String>,
+}
+
+pub fn get_google_client() -> BasicClient {
+    let client_id = ClientId::new(
+        env::var("GOOGLE_CLIENT_ID").expect("Missing the GOOGLE_CLIENT_ID environment variable."),
+    );
+    let client_secret = ClientSecret::new(
+        env::var("GOOGLE_CLIENT_SECRET")
+            .expect("Missing the GOOGLE_CLIENT_SECRET environment variable."),
+    );
+    let auth_url = AuthUrl::new("https://accounts.google.com/o/oauth2/v2/auth".to_string())
+        .expect("Invalid authorization endpoint URL");
+    let token_url = TokenUrl::new("https://www.googleapis.com/oauth2/v4/token".to_string())
+        .expect("Invalid token endpoint URL");
+
+    // Set up the config for the Google OAuth2 process.
+    BasicClient::new(client_id, Some(client_secret), auth_url, Some(token_url)).set_redirect_uri(
+        RedirectUrl::new("https://hoplite-challenge.philolog.us/gauth".to_string())
+            .expect("Invalid redirect URL"),
+    )
+}
+
+pub fn get_apple_client() -> BasicClient {
+    let client_id = ClientId::new(
+        env::var("APPLE_CLIENT_ID").expect("Missing the APPLE_CLIENT_ID environment variable."),
+    );
+    let client_secret = ClientSecret::new(
+        env::var("APPLE_CLIENT_SECRET")
+            .expect("Missing the APPLE_CLIENT_SECRET environment variable."),
+    );
+    let auth_url = AuthUrl::new("https://appleid.apple.com/auth/authorize".to_string())
+        .expect("Invalid authorization endpoint URL");
+    let token_url = TokenUrl::new("https://appleid.apple.com/auth/token".to_string())
+        .expect("Invalid token endpoint URL");
+
+    // Set up the config for the Apple OAuth2 process.
+    BasicClient::new(client_id, Some(client_secret), auth_url, Some(token_url)).set_redirect_uri(
+        RedirectUrl::new("https://hoplite-challenge.philolog.us/auth".to_string())
+            .expect("Invalid redirect URL"),
+    )
+}
+
+pub async fn oauth_login_apple(
+    (session, req): (Session, HttpRequest),
+) -> Result<HttpResponse, AWError> {
+    let data = req.app_data::<AppState>().unwrap();
+
+    let (pkce_code_challenge, _pkce_code_verifier) = PkceCodeChallenge::new_random_sha256();
+    let nonce = uuid::Uuid::new_v4(); // use UUID as random and unique nonce
+
+    let (authorize_url, csrf_state) = &data
+        .apple_oauth
+        .authorize_url(CsrfToken::new_random)
+        .set_response_type(&ResponseType::new("code id_token".to_string()))
+        .add_extra_param("response_mode".to_string(), "form_post".to_string())
+        .add_extra_param("nonce".to_string(), nonce.to_string())
+        .add_scope(Scope::new("openid".to_string()))
+        .add_scope(Scope::new("name".to_string()))
+        .add_scope(Scope::new("email".to_string()))
+        .set_pkce_challenge(pkce_code_challenge) //apple does not support this, but no problem including it
+        .url();
+
+    let state = csrf_state.secret().to_string();
+    session.renew();
+    session
+        .insert::<String>("oauth_state", state)
+        .expect("session.insert state");
+
+    Ok(HttpResponse::Found()
+        .append_header((header::LOCATION, authorize_url.to_string()))
+        .finish())
+}
+
+pub async fn oauth_login_google(
+    (session, req): (Session, HttpRequest),
+) -> Result<HttpResponse, AWError> {
+    let data = req.app_data::<AppState>().unwrap();
+    // Google supports Proof Key for Code Exchange (PKCE - https://oauth.net/2/pkce/).
+    // Create a PKCE code verifier and SHA-256 encode it as a code challenge.
+    let (pkce_code_challenge, _pkce_code_verifier) = PkceCodeChallenge::new_random_sha256();
+    let nonce = uuid::Uuid::new_v4(); // use UUID as random and unique nonce
+
+    let (authorize_url, csrf_state) = &data
+        .google_oauth
+        .authorize_url(CsrfToken::new_random)
+        .set_response_type(&ResponseType::new("code id_token".to_string()))
+        .add_extra_param("response_mode".to_string(), "form_post".to_string())
+        .add_extra_param("nonce".to_string(), nonce.to_string())
+        .add_scope(Scope::new("openid".to_string()))
+        // .add_scope(Scope::new("name".to_string()))
+        .add_scope(Scope::new("email".to_string()))
+        .set_pkce_challenge(pkce_code_challenge) // apple does not support this
+        .url();
+
+    let state = csrf_state.secret().to_string();
+    session.renew();
+    session
+        .insert::<String>("oauth_state", state)
+        .expect("session.insert state");
+
+    Ok(HttpResponse::Found()
+        .append_header((header::LOCATION, authorize_url.to_string()))
+        .finish())
+}
+
+pub async fn oauth_auth_apple(
+    (session, params, req): (Session, web::Form<AuthRequest>, HttpRequest),
+) -> Result<HttpResponse, AWError> {
+    let db = req.app_data::<HcDbPostgres>().unwrap();
+    let data = req.app_data::<AppState>().unwrap();
+
+    let saved_state = session.get::<String>("oauth_state").unwrap();
+
+    if let Some(param_code) = &params.code {
+        let code = AuthorizationCode::new(param_code.clone());
+        let received_state = CsrfToken::new(params.state.clone());
+        let user = params.user.clone();
+        let id_token = params.id_token.clone();
+
+        let _token = &data.apple_oauth.exchange_code(code);
+
+        if let Some(ref id_token_ref) = id_token {
+            if saved_state.unwrap() == *received_state.secret() {
+                let mut first_name = String::from("");
+                let mut last_name = String::from("");
+                let mut email = String::from("");
+
+                if let Some(ref user) = user {
+                    if let Ok(apple_oauth_user) = serde_json::from_str::<AppleOAuthUser>(user) {
+                        first_name = apple_oauth_user.name.first_name.unwrap_or(String::from(""));
+                        last_name = apple_oauth_user.name.last_name.unwrap_or(String::from(""));
+                        email = apple_oauth_user.email.unwrap_or(String::from(""));
+                    }
+                }
+
+                println!("apple test test3");
+                if let Ok(result) = sign_in_with_apple::validate::<AppleClaims>(
+                    &env::var("APPLE_CLIENT_ID")
+                        .expect("Missing the APPLE_CLIENT_ID environment variable."),
+                    id_token_ref,
+                    false,
+                    Issuer::APPLE,
+                )
+                .await
+                {
+                    let sub = result.claims.sub;
+                    let iss = result.claims.iss;
+                    email = result.claims.email.unwrap_or(String::from(""));
+
+                    let timestamp = libhc::get_timestamp();
+                    match hc_create_oauth_user(
+                        db,
+                        &iss,
+                        &sub,
+                        if email.is_empty() { None } else { Some(&email) },
+                        &first_name,
+                        &last_name,
+                        &email,
+                        timestamp,
+                    )
+                    .await
+                    {
+                        Ok((user_id, user_name)) => {
+                            session.renew(); //https://www.lpalmieri.com/posts/session-based-authentication-in-rust/#4-5-2-session
+                            if session.insert("user_id", user_id).is_ok()
+                                && session.insert("username", user_name).is_ok()
+                            {
+                                return Ok(HttpResponse::SeeOther()
+                                    .insert_header((LOCATION, "/"))
+                                    .finish());
+                            }
+                        }
+                        Err(Database(e)) => {
+                            FlashMessage::error(e.to_string()).send();
+                            return nav_to_login(session);
+                        }
+                        _ => (),
+                    }
+                }
+
+                return nav_to_login(session);
+            }
+        }
+    }
+
+    nav_to_login(session)
+}
+
+pub async fn oauth_auth_google(
+    session: Session,
+    State(db): State<HcDbPostgres>,
+    extract::Form(form): extract::Form<AuthRequest>,
+    //(session, params, req): (Session, web::Form<AuthRequest>, HttpRequest),
+) -> impl IntoResponse {
+    //let db = req.app_data::<HcDbPostgres>().unwrap();
+    let data = req.app_data::<AppState>().unwrap();
+    let saved_state = session.get::<String>("oauth_state").unwrap();
+
+    if let Some(param_code) = &params.code {
+        // println!("code code");
+        let code = AuthorizationCode::new(param_code.clone());
+        let received_state = CsrfToken::new(params.state.clone());
+        //let user = params.user.clone(); //google doesn't send user this way
+        let id_token = params.id_token.clone();
+
+        // Exchange the code with a token.
+        let _token = &data.google_oauth.exchange_code(code);
+
+        if let Some(ref id_token_ref) = id_token {
+            if saved_state.unwrap() == *received_state.secret() {
+                let first_name = String::from("");
+                let last_name = String::from("");
+                let mut email = String::from("");
+
+                println!("cccccc {:?}", id_token_ref);
+                println!("google test test3");
+                if let Ok(result) = sign_in_with_apple::validate::<GoogleClaims>(
+                    &env::var("GOOGLE_CLIENT_ID")
+                        .expect("Missing the GOOGLE_CLIENT_ID environment variable."),
+                    id_token_ref,
+                    false,
+                    Issuer::GOOGLE,
+                )
+                .await
+                {
+                    let sub = result.claims.sub;
+                    let iss = result.claims.iss;
+                    email = result.claims.email.unwrap_or(String::from(""));
+
+                    let timestamp = libhc::get_timestamp();
+
+                    match hc_create_oauth_user(
+                        db,
+                        &iss,
+                        &sub,
+                        if email.is_empty() { None } else { Some(&email) },
+                        &first_name,
+                        &last_name,
+                        &email,
+                        timestamp,
+                    )
+                    .await
+                    {
+                        Ok((user_id, user_name)) => {
+                            if session.insert("user_id", user_id).is_ok()
+                                && session.insert("username", user_name).is_ok()
+                            {
+                                return Redirect::to("/");
+                            }
+                        }
+                        Err(Database(e)) => {
+                            //FlashMessage::error(e.to_string()).send();
+                            return nav_to_login(session);
+                        }
+                        _ => (),
+                    }
+                }
+
+                return nav_to_login(session);
+            }
+        }
+    }
+
+    nav_to_login(session)
+}
+
+fn nav_to_login(session: Session) -> impl IntoResponse {
+    session.clean();
+    Redirect::to("/login")
+}
+*/
